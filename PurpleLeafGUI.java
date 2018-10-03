859c586278cd73407c8d25ff0fd1d40b9b8c7725
nureinpanda <43478445+nureinpanda@users.noreply.github.com>
2018-10-03 19:58:06 +0200

    Add files via upload

--- PurpleLeafGUI.java
+++ PurpleLeafGUI.java
@@ -1,14 +1,18 @@
 import java.awt.BorderLayout;
-import java.awt.MenuItem;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
+
+import java.awt.event.*;
 import java.io.IOException;
 
 import javax.swing.*;
-
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
 
 public class PurpleLeafGUI extends JFrame{
 	
+	JEditorPane browser = new JEditorPane();
 	
 	public PurpleLeafGUI() {	
 		BorderLayout layout = new BorderLayout();
@@ -18,13 +22,28 @@ public class PurpleLeafGUI extends JFrame{
 		setExtendedState(MAXIMIZED_BOTH);	
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		System.out.println();
-		
-		JEditorPane browser = new JEditorPane();
-		browser.setBounds(0, 25, 1000, 1000);
-		
+		/*
+		 * BROWSER
+		 */		
 		browser.setEditable(false);
 		
+		try {
+			browser.setPage("https://www.google.com/");
+		} catch (IOException e) {
+			System.out.println("Flascher Link");
+			e.printStackTrace();
+		}
+		browser.addHyperlinkListener(new HyperlinkListener() {
+			@Override
+			public void hyperlinkUpdate(HyperlinkEvent event) {
+				HyperlinkEvent.EventType typ = event.getEventType();
+
+				if (typ == HyperlinkEvent.EventType.ACTIVATED) {					
+						openLink(event.getURL().toString());					
+				}
+			}
+		});
+			
 		/*
 		 * menübar
 		 */
@@ -34,32 +53,79 @@ public class PurpleLeafGUI extends JFrame{
 		/*
 		 * Buttons
 		 */
-		JButton back = new JButton("BACK");
-		JButton forward = new JButton("FORWARD");
-		JButton F5 = new JButton("RELOAD");	
+		JButton back = new JButton("⇚");
+		JButton forward = new JButton("⇛");
+		
+		JButton F5 = new JButton("↻");	
+		F5.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openLink(browser.getPage().toString());			
+			}
+		});
 		
 		/*
 		 * adressbar
 		 */
 		JTextField adressbar = new JTextField();
+		adressbar.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) { }
+			
+			@Override
+			public void keyReleased(KeyEvent e) { }
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyChar() == KeyEvent.VK_ENTER){
+					openLink(adressbar.getText());
+				}
+				
+			}
+		});
+		adressbar.setFocusAccelerator('s');
+		adressbar.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				adressbar.selectAll();
+				
+			}
+		});
+		
+		adressbar.setFocusAccelerator('s');
+		adressbar.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				adressbar.selectAll();
+				
+			}
+		});
 		
 		/*
 		 * Menü
-		 */
-	
-		
-		JMenu options = new JMenu("Einstellungen");
+		 */		
+		JMenu options = new JMenu("≡");
 		JMenuItem neuertab = new JMenuItem("Neuer Tab");
 		options.add(neuertab);
 		
 		/*
 		 * Scrollbar
 		 */
-		JScrollPane scrollbar = new JScrollPane(browser);
-		add(scrollbar);
-		
-		
-		
+		JScrollPane bar = new JScrollPane(browser);	
+				
 		menubar.add(back);
 		menubar.add(forward);
 		menubar.add(F5);
@@ -67,9 +133,26 @@ public class PurpleLeafGUI extends JFrame{
 		menubar.add(options);
 		
 		add(menubar, BorderLayout.PAGE_START);
-		
-		add(browser, BorderLayout.CENTER);
+		add(bar , BorderLayout.CENTER);
 		
 		setVisible(true);
+	}
+	
+	public void openLink(String link) {
+		try {
+			
+			if(link.startsWith("https://")) {
+				browser.setPage(link);
+			}else {
+				browser.setPage("https://" + link);
+				setTitle("PurpleLeaf - " + link);			}
+			setTitle("PurpleLeaf - " + browser.getPage().toString());
+			
+			
+			
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(browser, "Seite Nicht Gefunden", "Error 404", JOptionPane.OK_CANCEL_OPTION);			
+		}
 	}
 }
